55d816e804f56d016b199ad7ae0ed5fd361663e0
Jacc89 <laflacalex9@gmail.com>
2021-05-15 16:46:19 -0500

    inicio android

---
+++ app/src/main/java/com/example/application_supermark/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.application_supermark
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return javaClass.hashCode()
+    }
+}
---
+++ app/src/main/java/com/example/application_supermark/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.application_supermark
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
